450c8b5cc4136ebe71f1456c30b944d129c57c19
konan <miyagikonan.2023.kensyu@gmail.com>
2023-04-24 15:40:42 +0900

    commit1

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+public class Account {
+
+    String name;
+    String tel;
+    String email;
+    String address;
+
+    public Account(String name,String tel,String email,String address){
+        name = "";
+        tel = "";
+        email = "";
+        address = "";
+    }
+
+    public void getAccountInfo(){
+
+    }
+
+}
